d818e0305390dd1558f712f73c504b55f24606f4
Giselle Hoekveld <gisellehoekveld@gmail.com>
2022-09-07 22:30:22 -0300

    create categories repository

---
+++ src/repositories/CategoriesRepository.ts
@@ -0,0 +1,38 @@
+import { Category } from "../model/Category";
+
+interface ICreateCategoryDTO {
+  description: string;
+  name: string;
+}
+
+class CategoriesRepository {
+  private categories: Category[];
+
+  constructor() {
+    this.categories = [];
+  }
+
+  create({ description, name }: ICreateCategoryDTO): void {
+    const category = new Category();
+
+    Object.assign(category, {
+      created_at: new Date(),
+      description,
+      name,
+    });
+
+    this.categories.push(category);
+  }
+
+  list(): Category[] {
+    return this.categories;
+  }
+
+  findByName(name: string): Category {
+    const category = this.categories.find((category) => category.name === name);
+
+    return category;
+  }
+}
+
+export { CategoriesRepository };
--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,25 +1,28 @@
 import { Router } from "express";
 
-import { Category } from "../model/Category";
+import { CategoriesRepository } from "../repositories/CategoriesRepository";
 
 const categoriesRoutes = Router();
-
-const categories: Category[] = [];
+const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
 
-  const category = new Category();
+  const categoryAlreadyExists = categoriesRepository.findByName(name);
 
-  Object.assign(category, {
-    created_at: new Date(),
-    description,
-    name,
-  });
+  if (categoryAlreadyExists) {
+    return response.status(400).json({ error: "Category already exists!" });
+  }
 
-  categories.push(category);
+  categoriesRepository.create({ name, description });
 
-  return response.status(201).json({ category });
+  return response.status(201).send();
+});
+
+categoriesRoutes.get("/", (request, response) => {
+  const all = categoriesRepository.list();
+
+  return response.json(all);
 });
 
 export { categoriesRoutes };
