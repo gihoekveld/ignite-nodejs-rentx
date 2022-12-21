e50416ae0072727658e5d894771906130d47fca5
Giselle Hoekveld <gisellehoekveld@gmail.com>
2022-12-20 23:16:55 -0300

    create migration to alter column created_at on specifications table

---
+++ src/database/migrations/1671588917676-AlterSpecificationsAddCreateAtDefault.ts
@@ -0,0 +1,19 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class AlterSpecificationsAddCreateAtDefault1671588917676
+  implements MigrationInterface
+{
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE specifications
+            ALTER COLUMN created_at SET DEFAULT now()`
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE specifications
+            ALTER COLUMN created_at DROP DEFAULT`
+    );
+  }
+}
