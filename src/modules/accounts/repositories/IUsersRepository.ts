160ea4176bdc73aff2c8eeda2892d467b68756e0
Giselle Hoekveld <gisellehoekveld@gmail.com>
2022-12-20 22:23:58 -0300

    check if email already exists

--- src/modules/accounts/repositories/IUsersRepository.ts
+++ src/modules/accounts/repositories/IUsersRepository.ts
@@ -1,6 +1,8 @@
 import { ICreateUserDTO } from "../dtos/ICreateUserDTO";
+import { User } from "../entities/User";
 
 interface IUsersRepository {
+  findByEmail(email: string): Promise<User>;
   create(data: ICreateUserDTO): Promise<void>;
 }
 
