6a93238d672c06cafc7a210ecb9cd141fd91761c
Giselle Hoekveld <gisellehoekveld@gmail.com>
2022-12-21 00:06:17 -0300

    overwrite express request interface

--- src/middlewares/ensureAuthenticated.ts
+++ src/middlewares/ensureAuthenticated.ts
@@ -34,6 +34,10 @@ export async function ensureAuthenticated(
       throw new AppError("User does not exists!", 401);
     }
 
+    request.user = {
+      id: user_id,
+    };
+
     return next();
   } catch (err) {
     throw new AppError("Invalid token!", 401);
