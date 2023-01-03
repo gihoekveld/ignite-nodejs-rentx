5f90e9bb55a274b43175ce1444cfd33c028391fd
Giselle Hoekveld <gisellehoekveld@gmail.com>
2023-01-03 19:46:57 -0300

    migration: add primary key to users table

--- src/shared/infra/typeorm/migrations/1671585469742-CreateUsers.ts
+++ src/shared/infra/typeorm/migrations/1671585469742-CreateUsers.ts
@@ -9,6 +9,7 @@ export class CreateUsers1671582410955 implements MigrationInterface {
           {
             name: "id",
             type: "uuid",
+            isPrimary: true,
           },
           {
             name: "name",
