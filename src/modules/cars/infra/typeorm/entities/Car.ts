a97a33ee9d4a41df4a21516f4646fc7171ba47a9
Giselle Hoekveld <gisellehoekveld@gmail.com>
2023-01-02 22:10:37 -0300

    implement typeorm on Car entity

--- src/modules/cars/infra/typeorm/entities/Car.ts
+++ src/modules/cars/infra/typeorm/entities/Car.ts
@@ -1,31 +1,41 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from "typeorm";
 import { v4 as uuidV4 } from "uuid";
 
+@Entity("cars")
 class Car {
+  @PrimaryColumn()
   id?: string;
 
+  @Column()
   name: string;
 
+  @Column()
   description: string;
 
+  @Column()
   daily_rate: number;
 
-  available: boolean;
+  @Column()
+  available = true;
 
+  @Column()
   license_plate: string;
 
+  @Column()
   fine_amount: number;
 
+  @Column()
   brand: string;
 
+  @Column()
   category_id: string;
 
+  @CreateDateColumn()
   created_at: Date;
 
   constructor() {
     if (!this.id) {
       this.id = uuidV4();
-      this.available = true;
-      this.created_at = new Date();
     }
   }
 }
